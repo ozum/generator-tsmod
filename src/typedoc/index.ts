3ab3e4b3dc68ef7a09409ab4e5139f992e003a61
Özüm Eldoğan <ozum@ozum.net>
2020-12-27 22:57:14 +0300

    fix: fix typedoc command

--- src/typedoc/index.ts
+++ src/typedoc/index.ts
@@ -16,7 +16,7 @@ export default class extends BaseGenerator<Options> {
 
     const typeDocCommand = [
       "rm -rf api-docs-md",
-      `typedoc ${vuepress} --plugin typedoc-plugin-example-tag,typedoc-plugin-markdown --excludeExternals --excludePrivate --excludeProtected --excludeNotExported --hideIndexes --theme markdown --readme none --mode file --out api-docs-md`,
+      `typedoc ${vuepress} --plugin typedoc-plugin-example-tag,typedoc-plugin-markdown --excludeExternals --excludePrivate --excludeProtected --excludeNotExported --exclude 'src/bin/**/*' --theme markdown --readme none --mode file --out api-docs-md`,
       'find api-docs-md -name "index.md" -exec sh -c \'mv "$1" "${1%index.md}"index2.md\' - {} \\;', // eslint-disable-line no-template-curly-in-string
     ];
 
