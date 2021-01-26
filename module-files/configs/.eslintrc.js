2a011999bf2156aebad4dec58e0e0644c396daf6
Özüm Eldoğan <ozum@ozum.net>
2021-01-26 16:39:03 +0300

    fix: reorder eslint rules

--- module-files/configs/.eslintrc.js
+++ module-files/configs/.eslintrc.js
@@ -16,12 +16,12 @@ module.exports = {
         },
       },
       extends: [
+        "airbnb-base", // TURN ON airbnb-base rules.
+        "plugin:jest/recommended", // TURN ON Jest rules by using "jest-community/eslint-plugin-jest".
+        "plugin:prettier/recommended", // RUN Prettier as ESLint rule by using `prettier/eslint-plugin-prettier` and TURN OFF ESLint rules which conflict with Prettier by using `prettier/eslint-config-prettier`.
         "eslint:recommended", // TURN ON ESLint recommended rules.
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended", // TURN ON TypeScript rules by using `typescript-eslint/typescript-eslint`.
-        "plugin:jest/recommended", // TURN ON Jest rules by using "jest-community/eslint-plugin-jest".
-        "airbnb-base", // TURN ON airbnb-base rules.
-        "plugin:prettier/recommended", // RUN Prettier as ESLint rule by using `prettier/eslint-plugin-prettier` and TURN OFF ESLint rules which conflict with Prettier by using `prettier/eslint-config-prettier`.
         "prettier/@typescript-eslint", // TURN OFF ESLint TypesSript rules which conflict with Prettier by using `prettier/eslint-config-prettier`.
       ],
       plugins: ["@typescript-eslint"],
